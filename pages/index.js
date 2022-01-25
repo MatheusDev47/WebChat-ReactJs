31d3b3efc55747de31aed9e22afc1a207a1f255d
Matheus Costa <97363297+MatheusDev47@users.noreply.github.com>
2022-01-25 11:43:59 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -196,7 +196,7 @@ export default function HomePage () {
                     {/* Photo Area */}
                 </Box>
 
-                <Image styleSheet={{width: {lg: '50%'}, display: {xl: 'block', xs: 'none'}}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" />
+                <Image styleSheet={{width: {lg: '40%'}, display: {xl: 'block', xs: 'none'}}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" />
 
             </Box>
         </>
